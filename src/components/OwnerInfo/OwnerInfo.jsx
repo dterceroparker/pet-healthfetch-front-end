577b92ffdb1f65e86c0c55309e70c2b00411894a
Marie Blakely <marieblakely@myyahoo.com>
2024-05-11 10:23:29 -0400

    add authoinfo, datecard, icon, landing, and logo

---
+++ src/components/OwnerInfo/OwnerInfo.jsx
@@ -0,0 +1,23 @@
+// components
+import DateCard from '../DateCard/DateCard'
+// assets
+import profileIcon from '../../assets/icons/profile.png'
+// css
+import styles from './OwnerInfo.module.css'
+
+
+const OwnerInfo = ({ content }) => {
+  const photo = content.owner.photo ? content.owner.photo : profileIcon
+
+  return (
+    <div className={styles.container}>
+      <img src={photo} alt="The user's avatar" />
+      <section>
+        <h4>{content.owner.name}</h4>
+        <DateCard createdAt={content.createdAt} />
+      </section>
+    </div>
+  )
+}
+
+export default OwnerInfo
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,34 @@
+//assets
+import logo from '../../assets/branding/logo.png'
 // css
 import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
   return (
+    <>
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      <section className={styles.splash}>
+          <img src={logo} alt="A cute pet" />
+        </section>
+        <section className={styles.about}>
+          <header>
+            <h1>ABOUT US</h1>
+          </header>
+          <article>
+            <p>
+            Our app, Pet healthFetch, is The Best Choice for Your Pets Health Care Needs! No Need to Text with your Vet Anymore! 
+            With our app, you can effortlessly create a profile of your pet, and request a new visit right from the app. 
+            Our app allows users to keep track of their vet visit history and features a photo upload option in the appointment form! 
+            Pet healthfetch is the ultimate companion for pet owners—ensuring that their furry friends receive the best care possible-anytime, anywhere!
+            </p>
+          </article>
+        </section>
     </main>
+    <footer className={styles.footer}>
+        <p>© 2024 HEALTHFETCH INC. PET RIGHTS RESERVED</p>
+      </footer>
+    </>
   )
 }
 
